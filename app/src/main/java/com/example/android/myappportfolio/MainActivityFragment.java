a3a4585340081f72b6d9df73ba05afdccf97b201
sumeshthayil <sumeshta@gmail.com>
2016-04-14 00:19:02 +0530

    First submission of  My App Portfolio project

---
+++ app/src/main/java/com/example/android/myappportfolio/MainActivityFragment.java
@@ -0,0 +1,27 @@
+package com.example.android.myappportfolio;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+}
+
+
